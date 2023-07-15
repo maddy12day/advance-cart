3b859e94659d3dbf50248aeff9a9864b4c76948c
madhavDubey10 <madhav.dubey@aretove.com>
2023-07-15 14:14:24 +0530

    complete navbar no functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import {Button,Container,Nav, Navbar as NavbarBs} from 'react-bootstrap'
 import {NavLink} from 'react-router-dom'
 import Logo from '../assets/shopping_cart_FILL0_wght400_GRAD0_opsz48.svg'  
+import 'D:/advanceCart/src/style/Navbar.css'
+
 export function Navbar(){
     return<>
-        <NavbarBs className='bg-white shadow-sm mb-3 '>
+        <NavbarBs sticky='top' className='bg-white shadow-sm mb-3 '>
         <Container className='mr-auto'>
             <Nav>
                 <Nav.Link to='/' as= {NavLink}>Home</Nav.Link>
@@ -15,8 +17,11 @@ export function Navbar(){
                 <Nav.Link to='/about' as= {NavLink}>About</Nav.Link>
             </Nav>
             <Button style={{ width: "3rem", height: "3rem", position: "relative" }}
-            variant="outline-primary">
+            variant="outline-primary"
+            className='rounded-circle'>
                 <img src={Logo} alt="" />
+                <div className='rounded-circle bg-danger d-flex justify-content-center align-item-center nav-btn-style' 
+                >3</div>
             </Button>
         </Container>
         </NavbarBs>
